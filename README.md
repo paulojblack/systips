600d9a4f6012d5801a75521be124dfca1df0daf0
Paulo Black <pjblack@email.wm.edu>
2018-11-05 15:26:12 -0500

    basic with 5 tips

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # systips
+
+Greets you with linux related informational string, link to a useful utility, or shortcut upon logging into a new shell.
+
+Written in Golang. Under development
+
+## TODO
+- Unit tests!!!
+- Add meta-information to each tip, such as a subcategory (networking, efficiency etc.) and "level" (beginner, intermediate, advanced)
+- Look in to text formatting of stdout
---
+++ systips.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "time"
+)
+
+func main() {
+    tip := getRandomTip()
+
+    fmt.Println(tip)
+}
+
+func getRandomTip() string {
+    seed := rand.NewSource(time.Now().UnixNano())
+    randomizer := rand.New(seed)
+    tipArray := getTips()
+
+    tip := tipArray[randomizer.Intn(len(tipArray))]
+    return tip
+}
+
+func getTips() []string {
+    var tipArray = []string{
+        "To see all local users on your current machine, execute 'cut -d: -f1 /etc/passwd'",
+        "Execute 'whoami' to see which user you are currently logged in as",
+        "Install https://github.com/Russell91/sshrc to copy in an rc file of your own aliases and command anytime you log in to a new server through ssh.",
+        "Learn to use the basic shell builtins thoroughly! For example, put `alias ll='ls -lha'` in your ~/.bash_profile or ~/.bashrc and instead of just the usual contents " +
+        "of the passed in directory, you will see (-a) all files, including hidden (starting with .) files, (-l) long format, which is more easily readable and shows more information " +
+        ", and (-h) which when used with (-l) will list the file size in human readable units (i.e. Byte, Gigabyte etc.)",
+        "Execute `man ${command}` to see the manual entry for that command. For example, `man grep` will explain how to use the `grep` command",
+    }
+
+    return tipArray
+}
