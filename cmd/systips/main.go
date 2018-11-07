d04b33aa26432de6aca7ee4778339f8d9971adbf
Paulo Black <pjblack@email.wm.edu>
2018-11-07 16:01:33 -0500

    refactor tip array into tip struct

--- cmd/systips/main.go
+++ cmd/systips/main.go
@@ -6,6 +6,13 @@ import (
     "time"
 )
 
+type Tip struct {
+    id int
+    tags []string
+    level string
+    txt string
+}
+
 func main() {
     tip := GetRandomTip()
 
@@ -18,19 +25,45 @@ func GetRandomTip() string {
     tipArray := GetTips()
 
     tip := tipArray[randomizer.Intn(len(tipArray))]
-    return tip
+
+    return tip.txt
 }
 
-func GetTips() []string {
-    var tipArray = []string{
-        "To see all local users on your current machine, execute 'cut -d: -f1 /etc/passwd'",
-        "Execute 'whoami' to see which user you are currently logged in as",
-        "Install https://github.com/Russell91/sshrc to copy in an rc file of your own aliases and command anytime you log in to a new server through ssh.",
-        "Learn to use the basic shell builtins thoroughly! For example, put `alias ll='ls -lha'` in your ~/.bash_profile or ~/.bashrc and instead of just the usual contents " +
-        "of the passed in directory, you will see (-a) all files, including hidden (starting with .) files, (-l) long format, which is more easily readable and shows more information " +
-        ", and (-h) which when used with (-l) will list the file size in human readable units (i.e. Byte, Gigabyte etc.)",
-        "Execute `man ${command}` to see the manual entry for that command. For example, `man grep` will explain how to use the `grep` command",
+func GetTips() []Tip {
+    tips := []Tip{
+        Tip {
+            id: 1,
+            tags: []string{"bash", "cut", "builtins", "linux"},
+            level: "i",
+            txt: "To see all local users on your current machine, execute 'cut -d: -f1 /etc/passwd'",
+        },
+        Tip {
+            id: 2,
+            tags: []string{"bash","builtins"},
+            level: "i",
+            txt: "Execute 'whoami' to see which user you are currently logged in as",
+        },
+        Tip {
+            id: 3,
+            tags: []string{"tag1", "tag2"},
+            level: "a",
+            txt: "Install https://github.com/Russell91/sshrc to copy in an rc file of your own aliases and command anytime you log in to a new server through ssh.",
+        },
+        Tip {
+            id: 4,
+            tags: []string{"tag1", "tag2"},
+            level: "b",
+            txt: "Learn to use the basic shell builtins thoroughly! For example, put `alias ll='ls -lha'` in your ~/.bash_profile or ~/.bashrc and instead of just the usual contents " +
+            "of the passed in directory, you will see (-a) all files, including hidden (starting with .) files, (-l) long format, which is more easily readable and shows more information " +
+            ", and (-h) which when used with (-l) will list the file size in human readable units (i.e. Byte, Gigabyte etc.)",
+        },
+        Tip {
+            id: 5,
+            tags: []string{"tag1", "tag2"},
+            level: "b",
+            txt: "Execute `man ${command}` to see the manual entry for that command. For example, `man grep` will explain how to use the `grep` command",
+        },
     }
 
-    return tipArray
+    return tips
 }
